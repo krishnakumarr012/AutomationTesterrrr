bda518b86a5556bab22e93052a7c0f82b36802c8
krishnakumarr012 <testMailid0012@gmail.com>
2023-10-25 20:36:40 +0530

    Second Project

---
+++ src/test/java/com/sts/Sample012.java
@@ -0,0 +1,18 @@
+package com.sts;
+
+public class Sample012 {
+	
+	private void login() {
+		System.out.println("Login success");
+}
+	
+	private void productSearch() {
+		System.out.println("Search success");
+}
+	
+	private void paymentMethod() {
+		System.out.println("Payment success");
+
+	}
+
+}
